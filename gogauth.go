70b0c821e3abcc115280ad7a193a715ce0255dd0
tanaikech <tanaike@hotmail.com>
2020-01-03 12:35:02 +0900

    Updated to v2.0.2

--- gogauth.go
+++ gogauth.go
@@ -12,25 +12,28 @@ import (
 func main() {
 	app := cli.NewApp()
 	app.Name = "gogauth"
-	app.Author = "tanaike [ https://github.com/tanaikech/gogauth ] "
-	app.Email = "tanaike@hotmail.com"
-	app.Usage = "Retrieves accesstoken for using Drive API from Google."
-	app.Version = "2.0.1"
-	app.Commands = []cli.Command{
+	app.Authors = []*cli.Author{
+		{Name: "tanaike [ https://github.com/tanaikech/gogauth ] ", Email: "tanaike@hotmail.com"},
+	}
+	app.UsageText = "Retrieves accesstoken for using Drive API from Google."
+	app.Version = "2.0.2"
+	app.Commands = []*cli.Command{
 		{
 			Name:    "getaccesstoken",
 			Aliases: []string{"g"},
 			Usage:   "Get accesstoken",
 			Action:  getTokens,
 			Flags: []cli.Flag{
-				cli.BoolFlag{
-					Name:  "reauth, r",
-					Usage: "Retrieve access and refresh tokens. If you changed scopes, please use this.",
+				&cli.BoolFlag{
+					Name:    "reauth, r",
+					Aliases: []string{"r"},
+					Usage:   "Retrieve access and refresh tokens. If you changed scopes, please use this.",
 				},
-				cli.IntFlag{
-					Name:  "port, p",
-					Usage: "Port number of temporal web server for retrieving authorization code.",
-					Value: 8080,
+				&cli.IntFlag{
+					Name:    "port, p",
+					Aliases: []string{"p"},
+					Usage:   "Port number of temporal web server for retrieving authorization code.",
+					Value:   8080,
 				},
 			},
 		},
@@ -40,9 +43,10 @@ func main() {
 			Usage:   "Check accesstoken",
 			Action:  checkAccesstoken,
 			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "accesstoken, a",
-					Usage: "Confirm condition of current accesstoken",
+				&cli.StringFlag{
+					Name:    "accesstoken, a",
+					Aliases: []string{"a"},
+					Usage:   "Confirm condition of current accesstoken",
 				},
 			},
 		},
